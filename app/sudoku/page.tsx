904bedd0574137afefbc7a79ab8bfab99de47bc4
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-05-17 12:44:11 +0300

    sudoku page initialization

---
+++ app/sudoku/page.tsx
@@ -0,0 +1,10 @@
+import { title } from "@/components/primitives";
+
+export default function DocsPage() {
+  return (
+    <div>
+      <h1 className={title()}>Sudoku</h1>
+      <div className="grid grid-cols-9">{}</div>
+    </div>
+  );
+}
