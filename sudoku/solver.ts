773911b7b288d75c77143603c64832c997356b3a
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-05-27 13:06:47 +0300

    refactor solver

--- sudoku/solver.ts
+++ sudoku/solver.ts
@@ -7,45 +7,64 @@ import type {
 } from "@/types/sudokuTypes";
 import { getBoxes, getColumns } from "./sudokuHelpers";
 
-const isSafe = (
-	grid: SudokuGrid,
-	row: number,
-	col: number,
-	num: number,
-): boolean => {
-	// Check if num is not in the current row, column and 3x3 subgrid
-	for (let x = 0; x < 9; x++) {
-		if (grid[row][x] === num || grid[x][col] === num) {
-			return false;
-		}
-	}
+// const isSafe = (
+// 	grid: SudokuGrid,
+// 	row: number,
+// 	col: number,
+// 	num: number,
+// ): boolean => {
+// 	// Check if num is not in the current row, column and 3x3 subgrid
+// 	for (let x = 0; x < 9; x++) {
+// 		if (grid[row][x] === num || grid[x][col] === num) {
+// 			return false;
+// 		}
+// 	}
 
-	const startRow = row - (row % 3);
-	const startCol = col - (col % 3);
-	for (let i = 0; i < 3; i++) {
-		for (let j = 0; j < 3; j++) {
-			if (grid[i + startRow][j + startCol] === num) {
-				return false;
-			}
-		}
+// 	const startRow = row - (row % 3);
+// 	const startCol = col - (col % 3);
+// 	for (let i = 0; i < 3; i++) {
+// 		for (let j = 0; j < 3; j++) {
+// 			if (grid[i + startRow][j + startCol] === num) {
+// 				return false;
+// 			}
+// 		}
+// 	}
+
+// 	return true;
+// };
+
+export const checkCompletedSudoku = (grid: SudokuGrid): boolean => {
+	if (!checkAllRows(grid) || !checkAllColumns(grid) || !checkAllBoxes(grid)) {
+		return false;
 	}
 
 	return true;
 };
 
-export const checkCompletedSudoku = (grid: SudokuGrid): boolean => {
+export const checkAllRows = (grid: SudokuGrid): boolean => {
+	for (let i = 0; i < 9; i++) {
+		if (!checkSudokuRowOrColumn(grid[i])) {
+			console.log(`sudoku not completable in row ${i} with values ${grid[i]}`);
+			return false;
+		}
+	}
+	return true;
+};
+
+export const checkAllColumns = (grid: SudokuGrid): boolean => {
 	const columns = getColumns(grid);
 	for (let i = 0; i < 9; i++) {
-		if (
-			!checkSudokuRowOrColumn(grid[i]) ||
-			!checkSudokuRowOrColumn(columns[i])
-		) {
+		if (!checkSudokuRowOrColumn(columns[i])) {
 			console.log(
-				`sudoku not completable in grid number ${i} with values ${grid[i]} or column number ${i} with values ${columns[i]}`,
+				`sudoku not completable in column number ${i} with values ${columns[i]}`,
 			);
 			return false;
 		}
 	}
+	return true;
+};
+
+export const checkAllBoxes = (grid: SudokuGrid): boolean => {
 	const boxes = getBoxes(grid);
 	for (let i = 0; i < 3; i++) {
 		for (let j = 0; j < 3; j++) {
@@ -60,6 +79,8 @@ export const checkCompletedSudoku = (grid: SudokuGrid): boolean => {
 	return true;
 };
 
+//both row and column are identical, they are separated for possible future typing differences
+//and for functions that return columns instead of rows
 const checkSudokuRowOrColumn = (row: Row | Column): boolean => {
 	const rowSet = new Set<SudokuNumber>();
 	for (const cell of row) {
