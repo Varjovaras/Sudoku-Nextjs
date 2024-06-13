9e4a53918cd8e85d1e470e10475d0cdc13468c38
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-06-13 19:45:16 +0300

    tab goes from left to right

--- components/grid.tsx
+++ components/grid.tsx
@@ -1,46 +1,49 @@
 import type { SudokuGrid, SudokuNumber } from "@/types/sudokuTypes";
-import type { FC } from "react";
+import { useRef, type FC } from "react";
 import { SudokuSquare } from "./sudokuSquare";
 import { SudokuInput } from "./sudokuInput";
 
 import "../styles/gridStyles.css";
 
 interface GridProps {
-  sudoku: SudokuGrid;
-  updateSudoku: (i: number, j: number, num: SudokuNumber) => void;
+	sudoku: SudokuGrid;
+	updateSudoku: (i: number, j: number, num: SudokuNumber) => void;
 }
 
 export const Grid: FC<GridProps> = ({ sudoku, updateSudoku }) => {
-  return (
-    <div className="grid grid-cols-9 p-4 overflow-auto">
-      {sudoku.map((row, i) => (
-        <div
-          // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-          key={`row-${i}`}
-        >
-          {row.map((cell, j) => {
-            if (cell?.preArranged) {
-              return (
-                <SudokuSquare
-                  // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-                  key={`cell-${i}-${j}`}
-                  cell={cell}
-                  j={j}
-                />
-              );
-            }
-            return (
-              <SudokuInput
-                i={i}
-                j={j}
-                updateSudoku={updateSudoku}
-                // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-                key={`input-${i}-${j}`}
-              />
-            );
-          })}
-        </div>
-      ))}
-    </div>
-  );
+	const ref = useRef(null);
+
+	return (
+		<div className="p-4 overflow-auto">
+			{sudoku.map((row, i) => (
+				<div
+					className="grid  grid-cols-9 "
+					// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+					key={`row-${i}`}
+				>
+					{row.map((cell, j) => {
+						if (cell?.preArranged) {
+							return (
+								<SudokuSquare
+									// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+									key={`cell-${i}-${j}`}
+									cell={cell}
+									j={j}
+								/>
+							);
+						}
+						return (
+							<SudokuInput
+								i={i}
+								j={j}
+								updateSudoku={updateSudoku}
+								// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+								key={`input-${i}-${j}`}
+							/>
+						);
+					})}
+				</div>
+			))}
+		</div>
+	);
 };
--- sudoku/sudokuDifficultyGenerator.ts
+++ sudoku/sudokuDifficultyGenerator.ts
@@ -3,7 +3,7 @@ import type { SudokuGrid } from "@/types/sudokuTypes";
 export const makeSudokuEasy = (sudoku: SudokuGrid): SudokuGrid => {
   for (const row of sudoku) {
     for (const cell of row) {
-      if (Math.random() < 0.2) {
+      if (Math.random() < 0.5) {
         cell.preArranged = false;
         cell.value = null;
       }
